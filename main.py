0afd62b9f16c17a76af55391be90d7e32dadf64f
Igor Deminski <kartoschka17gmail.com>
2023-08-02 15:55:47 +0300

    data validation with Pydantic

--- main.py
+++ main.py
@@ -1,41 +1,63 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI(
     title='Trading app'
 )
 
-users = [
-    {"id": 1, 'role': "admin", "name": "Marcin"},
-    {"id": 2, 'role': "investor", "name": "Gregorz"},
-    {"id": 3, 'role': "trader", "name": "Wojcech"}
-]
-
-
-@app.get('/users/{user_id}')
-def get_user(user_id: int):
-    return [user for user in users if user.get('id') == user_id]
-
-
-trades = [
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 423, "amount": 2.54},
-    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 542, "amount": 2.54},
-]
-
-
-@app.get('/trades')
-def get_trades(limit: int = 1, offset: int = 1):
-    return trades[offset:][:limit]
-
-
-users2 = [
+fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "investor", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]},
 ]
 
 
-@app.post("/users/{user_id}")
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, users2))[0]
-    current_user['name'] = new_name
-    return {"status": 200, "data": current_user}
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []
+
+
+@app.get("/users/{user_id}", response_model=List[User])
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get("id") == user_id]
+
+
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
+    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
+]
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
